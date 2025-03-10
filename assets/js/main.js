b806a0066f02154daaaa9d9b63803b39ac605906
KC Lopez <khalelclarklopez@gmail.com>
2025-03-10 03:32:48 -0700

    ADD: basic api fetching functionality
    the code and design looks so damn ugly sry guys lol. ill branch out after this commit to make smth nicer o7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 // main.js: Contains client-side logic for handling form submissions, UI updates, and interactions.
+
+let btn = document.getElementById("btn");
+btn.onclick = test;
+
+async function test(){
+    let data = await fetch("https://www.dnd5eapi.co/api/2014/monsters/adult-black-dragon/")
+    let dataTxt = await data.text();
+    let dataJSON = await JSON.parse(dataTxt);
+    console.log(dataJSON);
+
+    let node = document.createElement("p");
+    console.log(typeof(node));
+    node.innerHTML = dataJSON["name"];
+    document.body.appendChild(node);
+}
