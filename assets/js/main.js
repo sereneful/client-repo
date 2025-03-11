94a0d8a2e40af787cfb7b7054b9c00a136f03c11
KC Lopez <khalelclarklopez@gmail.com>
2025-03-11 11:35:01 -0700

    ADD: enemy card display + additonal css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // main.js: Contains client-side logic for handling form submissions, UI updates, and interactions.
 
 let btn = document.getElementById("btn");
+let div = document.getElementById("enemy-div");
 btn.onclick = test;
 
 async function test(){
@@ -9,8 +10,38 @@ async function test(){
     let dataJSON = await JSON.parse(dataTxt);
     console.log(dataJSON);
 
-    let node = document.createElement("p");
-    console.log(typeof(node));
-    node.innerHTML = dataJSON["name"];
-    document.body.appendChild(node);
+    let node = document.createElement("div");
+    let image = document.createElement("img");
+    let name = document.createElement("p");
+    let cr = document.createElement("p");
+    let hp = document.createElement("p");
+    let ac = document.createElement("p");
+    let stats = document.createElement("div");
+
+    image.src = "https://www.dnd5eapi.co" + dataJSON["image"];
+    name.innerHTML = dataJSON["name"];
+    cr.innerHTML = "CR: " + dataJSON["challenge_rating"];
+    hp.innerHTML = "HP: " + dataJSON["hit_points"];
+    ac.innerHTML = "AC: " + getAC(dataJSON["armor_class"]);
+
+    node.appendChild(image);
+    node.appendChild(name);
+    stats.appendChild(cr);
+    stats.appendChild(hp);
+    stats.appendChild(ac);
+    node.appendChild(stats);
+
+    name.classList.add("enemy-title")
+    node.classList.add("enemy-card");
+    div.appendChild(node);
 }
+
+function getAC(arr){
+    let ac = 0;
+    arr.forEach(obj => {
+        ac += obj["value"];
+    });
+    return ac;
+}
+
+test();
